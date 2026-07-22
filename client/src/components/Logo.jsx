ffe0645400a6dd82a7ffa69271732b8ee5e9a012
Ajay Rathore <ajayrathoref@gmail.com>
2026-07-22 22:29:21 +0530

    changes

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width='100px'}) {
   return (
-    <div className='font-semibold'  >BLOGCRAFT</div>
+    <div className='font-semibold'>ContentHub</div>
   )
 }
 
